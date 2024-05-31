404c9fdb7fa52cd157b73a67bad804ca8f8c023c
wgseligman <seligman@nevis.columbia.edu>
2024-05-31 16:50:57 -0400

    MCTrackList operator<< in .cc file

--- GramsDataObj/include/MCTrackList.h
+++ GramsDataObj/include/MCTrackList.h
@@ -80,26 +80,10 @@ namespace grams {
 
   }; // MCTrajectoryPoint 
 
-  // I prefer to include "write" operators in my custom classes to
+  // I prefer to define "write" operators in my custom classes to
   // make it easier to examine their contents.  In order to print an
   // MCTrajectoryPoint:
-  std::ostream& operator<< (std::ostream& out, const MCTrajectoryPoint& tp) {
-    out << "(x,y,z,t)=(" 
-	<< std::setprecision(5) << std::right
-	<< std::setw(11) << tp.x() << std::setw(1) << "," 
-	<< std::setw(11) << tp.y() << std::setw(1) << "," 
-	<< std::setw(11) << tp.z() << std::setw(1) << "," 
-	<< std::setw(11) << tp.t() << std::setw(0)
-	<< ") (px,py,pz,E)=("
-	<< std::setprecision(5) << std::right
-	<< std::setw(11) << tp.px() << std::setw(1) << "," 
-	<< std::setw(11) << tp.py() << std::setw(1) << "," 
-	<< std::setw(11) << tp.pz() << std::setw(1) << "," 
-	<< std::setw(11) << tp.E() << std::setw(0)
-	<< ") id="
-	<< tp.identifier();
-    return out;
-  }
+  std::ostream& operator<< (std::ostream& out, const MCTrajectoryPoint& tp);
 
   // A trajectory is a sequence of trajectory points. It's not likely
   // we'll have two trajectory points in a track with the same time
@@ -255,26 +239,6 @@ namespace grams {
     friend std::ostream& operator<< (std::ostream& out, const MCTrack& m);
 
   }; // MCTrack
-
-  // The full definition of MCTrack::operator<<. 
-  std::ostream& operator<< (std::ostream& out, const MCTrack& track) {
-    out << "Track ID " << track.trackID
-	<< " PDG code " << track.pdgCode
-	<< " parent ID " << track.parentID
-	<< " process (start) " << track.process
-	<< " process (end) " << track.endProcess
-	<< std::endl;
-    out << "   daughters:";
-    for ( const auto& d : track.daughters ) {
-      out << " " << d;
-    }
-    out << std::endl;
-    out << "   trajectory points:" << std::endl;
-    for ( const auto& tp : track.trajectory ) {
-      out << tp << std::endl;
-    }
-    return out;
-  }
 
   // Define a list of tracks for an event.
   typedef std::set< MCTrack > MCTrackList;
