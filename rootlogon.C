7553908f943e1a0f402c912f3c07a60eb9e95e51
wgseligman <seligman@nevis.columbia.edu>
2024-06-17 07:59:30 -0400

    Interpreter ROOT with custom dictionary

---
+++ rootlogon.C
@@ -0,0 +1,14 @@
+// These lines are automatically executed when you start interactive
+// ROOT from this directory.
+
+// These lines assume you have created a GramsSim build directory
+// following the recipe in the documentation:
+// https://github.com/wgseligman/GramsSim/blob/develop/README.md and
+// are running the script with the above command in that directory. If
+// you've moved the script, then you'll have to edit the "gSystem"
+// lines.
+
+{
+  gSystem->AddIncludePath("../GramsSim/GramsDataObj/include");
+  gSystem->Load("GramsDataObj/libGramsSimProjectDataObj.so");
+}
--- scripts/SimpleAnalysis.C
+++ scripts/SimpleAnalysis.C
@@ -1,49 +1,79 @@
 // To run this ROOT macro, you can do something like this:
 //    root scripts/SimpleAnalysis.C
 
-{
-  // A very simple look at the ntuples produced by
+// This macro assumes you have created a GramsSim build directory
+// following the recipe in the documentation:
+// https://github.com/wgseligman/GramsSim/blob/develop/README.md and
+// are running the script with the above command in that directory. If
+// you've moved the script, then you'll have to copy and adjust the
+// contents of rootlogon.C.
+
+void SimpleAnalysis() {
+  // A very simple look at the tree produced by
   // gramsg4. 
 
-  gROOT->Reset();
-  TFile inputFile("gramsg4.root");
+  // Open the input file.
+  auto input = TFile::Open("gramsg4.root");
 
-  TNtuple* larhits = (TNtuple*)inputFile.Get("LArHits");
-  TNtuple* trackinfo = (TNtuple*)inputFile.Get("TrackInfo");
+  // Create a "reader" for the tree in the input file.
+  auto reader = new TTreeReader("gramsg4", input);
 
-  // This will be used as a selection term later on.
-  const TString findLArTracks("VolNameStart==\"volTPCActive_PV\"");
+  // Create a TTreeReaderValue for each column in the tree whose value
+  // we'll use. 
 
-  // The following assumes that 1000 events were generated in
-  // gramsg4. 
-  for ( int event = 0; event != 1000; ++event ) {
-    // Select only those events that had hits. Note that
-    // if the simulation was run in multi-threaded mode,
-    // the event numbers will _not_ be in ascending order.
-    TString selection = "Event==" + std::to_string(event);
-    TTree* subTree = larhits->CopyTree(selection);
+  // Note that the definitions for these custom classes (e.g.,
+  // "grams::EventID") come from the GramsDataObj dictionary; see
+  // GramsSim/GramsDataObj/README.md for details. They were loaded
+  // into the ROOT intepreter by the file rootlogon.C.
 
+  // Also note that these behave like pointers; e.g., you would use
+  // *EventID in the code below.
+  TTreeReaderValue<grams::EventID>     EventID     = {*reader, "EventID"};
+  TTreeReaderValue<grams::MCTrackList> mcTrackList = {*reader, "TrackList"};
+  TTreeReaderValue<grams::MCLArHits>   mcLArHits   = {*reader, "LArHits"};
+
+  // Read every entry (row) in the input tree. 
+  while ( (*reader).Next() ) {
+ 
     // Did this event have any LAr hits?
-    auto numLArHits = subTree->GetEntriesFast();
-
+    auto numLArHits = mcLArHits->size();
     if ( numLArHits > 0 ) {
-      // Get the corresponding track info for that event.
-      // Note that whether or not gramsg4 is run multi-threaded,
-      // the track IDs will not be numerically sorted. 
-      TTree* tracks = trackinfo->CopyTree(selection);
+
+      // How many tracks were in this event?
+      auto numTracks = mcTrackList->size();
+
+      // How many of those tracks were in the LAr? Look at each track
+      // in the list of Tracks.
+      int numLArTracks = 0;
+      for ( const auto& [ trackID, track ] : (*mcTrackList) ) {
+
+	// Look at the trajectory of this track.
+	auto trajectory = track.Trajectory();
+
+	// For this simple analysis, we want to look at tracks whose
+	// trajectories start in the LAr. Note that _none_ of the
+	// primary particles start in the LAr; they start outside the
+	// detector.
+	auto trajectoryPoint = trajectory[0];
+	auto identifier = trajectoryPoint.Identifier();
+
+	// In the Identifier scheme documented in GramsSim/grams.gdml,
+	// volume ID numbers in the LAr are seven-digit numbers that
+	// begin with 1.
+	auto volumeType = identifier / 1000000;
+	if ( volumeType == 1 ) 
+	  ++numLArTracks;
+
+      } // loop over tracks.
 
       // Display some counts, including those tracks that 
       // started in the active LAr. 
-      std::cout << "Event " << event << " has "
+      std::cout << "Event " << (*EventID) << " has "
 		<< numLArHits << " hits and "
-		<< tracks->GetEntriesFast() << " tracks; "
-		<< tracks->GetEntries(findLArTracks) 
+		<< numTracks << " tracks; "
+		<< numLArTracks 
 		<< " of these tracks started in the active LAr" << std::endl;
 
-      // Clean up. 
-      delete tracks;
-    }
-
-    delete subTree;
-  }
+    } // If there were hits
+  } // For each row in the tree
 }
