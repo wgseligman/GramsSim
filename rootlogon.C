b0e9a16c17e8808dde3a36bdef5b32b0bb658871
wgseligman <seligman@nevis.columbia.edu>
2024-07-09 15:57:51 -0400

    Update for include location

--- rootlogon.C
+++ rootlogon.C
@@ -1,18 +1,13 @@
 // These lines are automatically executed when you start interactive
 // ROOT from this directory.
 
-// These lines assume you have created a GramsSim build directory
-// following the recipe in the documentation:
-// https://github.com/wgseligman/GramsSim/blob/develop/README.md and
-// are running the script with the above command in that directory. If
-// you've moved the script, then you'll have to edit the "gSystem"
-// lines.
-
+// Let ROOT know about the header files in 'include' and the location
+// of the ROOT dictionary for GramsSim.
 {
-  gInterpreter->AddIncludePath("../GramsSim/GramsDataObj/include");
+  gInterpreter->AddIncludePath("include");
   // This line creates a text string containing the library name, with
   // the appropriate extension (.so or .dylib) for this operating
   // system.
-  TString lib(Form("./libDictionary.%s", gSystem->GetSoExt()));
+  TString lib(Form("libDictionary.%s", gSystem->GetSoExt()));
   gSystem->Load(lib.Data());
 }
